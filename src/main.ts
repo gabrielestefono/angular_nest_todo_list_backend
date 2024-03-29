dde7628cc3c9962bb395c6f025c30b972b0b5cdf
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-29 19:48:06 -0300

    Tentando mudar frontend

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,13 @@ import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors());
+  app.use(cors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+    allowedHeaders: 'Content-Type,Authorization',
+  }));
   await app.listen(3000);
 }
 bootstrap();
