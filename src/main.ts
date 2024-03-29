f4c8d3a73a4df7d0607553502c6560f17980ab04
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-29 20:11:40 -0300

    Teste 1

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  // Use o middleware CORS
-  app.use(cors());
-
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
