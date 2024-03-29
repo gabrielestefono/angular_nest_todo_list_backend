828a15dab4da0bb0a529b4030939cdcf2150413e
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-03-29 19:51:33 -0300

    Removendo middleware para testes

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(cors({
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    allowedHeaders: 'Content-Type,Authorization',
-  }));
   await app.listen(3000);
 }
 bootstrap();
