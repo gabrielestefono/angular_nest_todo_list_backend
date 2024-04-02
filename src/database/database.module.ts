e1461293a18efc13dbeffc8cad6ed67acbb9c033
Gabriel Estéfono <gabrielestefono@hotmail.com>
2024-04-02 10:07:31 -0300

    Trocando banco de dados

--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -1,25 +1,22 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Task } from 'src/task/entity/task.entity';
-import { DataSourceOptions } from 'typeorm';
-
-export const dataSourceOptions: DataSourceOptions = {
-  type: 'sqlite',
-  database: 'dados.db',
-  entities: [Task],
-  synchronize: true,
-};
 
 @Module({
-	imports: [
-    TypeOrmModule.forRootAsync({
-      useFactory: () => {
-        return {
-          ...dataSourceOptions
-        };
-      }
-    })
-  ]
+ imports: [
+   TypeOrmModule.forRootAsync({
+     useFactory: () => ({
+       type: 'postgres',
+       host: process.env.POSTGRES_HOST,
+       port: 5432,
+       username: process.env.POSTGRES_USER,
+       password: process.env.POSTGRES_PASSWORD,
+       database: process.env.POSTGRES_DATABASE,
+       entities: [Task],
+       synchronize: true,
+       ssl: true,
+     }),
+   }),
+ ],
 })
-
 export class DatabaseModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 async function bootstrap() {
+  console.log(process.env.POSTGRES_DATABASE)
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({
